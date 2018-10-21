6e2b6f8d575cc4ccaf7c87ec25eec305770252b1
laoa <xiaoqikillua@sina.com>
2018-10-21 11:19:56 +0700

    添加今日头条适配方案。

---
+++ app/src/main/java/example/com/templateprogram/utils/DensityUtils.java
@@ -0,0 +1,94 @@
+package example.com.templateprogram.utils;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by beijixiong on 2018/10/13.
+ * 通过修改系统参数来适配android设备（今日头条适配方案）
+ */
+
+public class DensityUtils {
+
+    private static float appDensity;
+    private static float appScaledDensity;
+    private static DisplayMetrics appDisplayMetrics;
+    private static int barHeight;
+
+    public static void setDensity(@NonNull final Application application) {
+        //获取application的DisplayMetrics
+        appDisplayMetrics = application.getResources().getDisplayMetrics();
+        //获取状态栏高度
+        barHeight = BarUtils.getStatusBarHeight(application);
+
+        if (appDensity == 0) {
+            //初始化的时候赋值
+            appDensity = appDisplayMetrics.density;
+            appScaledDensity = appDisplayMetrics.scaledDensity;
+
+            //添加字体变化的监听
+            application.registerComponentCallbacks(new ComponentCallbacks() {
+                @Override
+                public void onConfigurationChanged(Configuration newConfig) {
+                    //字体改变后,将appScaledDensity重新赋值
+                    if (newConfig != null && newConfig.fontScale > 0) {
+                        appScaledDensity = application.getResources().getDisplayMetrics().scaledDensity;
+                    }
+                }
+
+                @Override
+                public void onLowMemory() {
+                }
+            });
+        }
+    }
+
+
+    //此方法在BaseActivity中做初始化(如果不封装BaseActivity的话,直接用下面那个方法就好)
+    public static void setDefault(Activity activity) {
+        setAppOrientation(activity, "width");
+    }
+
+    //此方法用于在某一个Activity里面更改适配的方向
+    public static void setOrientation(Activity activity, String orientation) {
+        setAppOrientation(activity, orientation);
+    }
+
+
+    /**
+     * targetDensity
+     * targetScaledDensity
+     * targetDensityDpi
+     * 这三个参数是统一修改过后的值
+     * <p>
+     * orientation:方向值,传入width或height
+     */
+    private static void setAppOrientation(Activity activity, String orientation) {
+
+        float targetDensity;
+
+        if (orientation.equals("height")) {
+            targetDensity = (appDisplayMetrics.heightPixels - barHeight) / 667f;
+        } else {
+            targetDensity = appDisplayMetrics.widthPixels / 360f;//以360为基础
+        }
+
+        float targetScaledDensity = targetDensity * (appScaledDensity / appDensity);
+        int targetDensityDpi = (int) (160 * targetDensity);
+
+        /**
+         *
+         * 最后在这里将修改过后的值赋给系统参数
+         *
+         * 只修改Activity的density值
+         */
+        DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
+        activityDisplayMetrics.density = targetDensity;
+        activityDisplayMetrics.scaledDensity = targetScaledDensity;
+        activityDisplayMetrics.densityDpi = targetDensityDpi;
+    }
+}
