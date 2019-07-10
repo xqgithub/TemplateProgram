28911d0ff011f0951dcf3425953562a20ad2057b
xqgithub <xiaoqikillua@sina.com>
2019-07-10 10:41:55 +0800

    TestEventBusActivityOne  添加 priority 注释

--- app/src/main/java/example/com/templateprogram/test/activity/TestEventBusActivityOne.java
+++ app/src/main/java/example/com/templateprogram/test/activity/TestEventBusActivityOne.java
@@ -53,6 +53,7 @@ public class TestEventBusActivityOne extends BaseActivity {
      * 2.ThreadMode.MAIN:如果使用事件处理函数指定了线程模型为MainThread，那么不论事件是在哪个线程中发布出来的，该事件处理函数都会在UI线程中执行
      * 3.ThreadMode.BACKGROUND:如果使用事件处理函数指定了线程模型为BackgroundThread，那么如果事件是在UI线程中发布出来的，那么该事件处理函数就会在新的线程中运行，如果事件本来就是子线程中发布出来的，那么该事件处理函数直接在发布事件的线程中执行。在此事件处理函数中禁止进行UI更新操作
      * 4.ThreadMode.ASYNC:如果使用事件处理函数指定了线程模型为Async，那么无论事件在哪个线程发布，该事件处理函数都会在新建的子线程中执行。同样，此事件处理函数中禁止进行UI更新操作。
+     * 5.priority 数字越大，级别越高，优先得到数据，默认是0
      *
      * @param event
      */
